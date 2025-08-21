f005f0cacd31ecba15382d238084612f4cd350e0
Dalavan123 <dalavan@hotmail.com>
2025-08-21 06:29:41 +0200

    Fått upp mockade filmer på UI homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,20 @@
+import { mockData } from '../components/App';
+import { mapMovie } from '../lib/tmdbAdapter';
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const movies = mockData.map(mapMovie);
+
+  return (
+    <div>
+      {movies.map(movie => (
+        <div key={movie.id}>
+          <img src={movie.posterUrl} alt={movie.title} />
+          <h3>
+            {movie.title} ({movie.year})
+          </h3>
+          <p>⭐ {movie.rating}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
