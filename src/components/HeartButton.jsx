4eba648467c4f96034ad40bca9efea6db2f0ff82
Adrian Jönsson <adrianjonsson@hotmail.com>
2025-08-26 08:51:31 +0200

    glömde spara en konfliktfilen

--- src/components/HeartButton.jsx
+++ src/components/HeartButton.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export function HeartButton({ onClick, ...props }) {
   return (
     <button
@@ -8,16 +7,6 @@ export function HeartButton({ onClick, ...props }) {
       data-cy="heart-btn"
       onClick={onClick}
       {...props}
-=======
-import './../styles/HeartButton.css';
-
-export function HeartButton({ onClick }) {
-  return (
-    <button
-      className='heart-button'
-      onClick={onClick}
-      aria-label='Add to Watchlist'
->>>>>>> 08a56e67b345fafe884db57a9e5af453a4505bc8
     >
       ❤️
     </button>
