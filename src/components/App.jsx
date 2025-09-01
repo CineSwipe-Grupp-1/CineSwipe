96e4ff9cda63baa590ccc536f694373e08fc7769
Zhel <maricelrefama@gmail.com>
2025-09-01 17:48:12 +0200

    Added header/logo and styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from './Navbar';
 import '../styles/App.css';
 import { Outlet } from 'react-router-dom';
+import  Header  from './Header';
 
 export function App() {
   return (
     <div className='app-layout'>
+      <Header />
       <main className='page-content'>
         <Outlet />
       </main>
